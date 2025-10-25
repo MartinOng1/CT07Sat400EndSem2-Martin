df822a4c091ce2d134f8f96db330460b742e129e
MartinOng1 <90502210+MartinOng1@users.noreply.github.com>
2025-10-25 16:23:43 +0800

    Update testquestion2.js

--- testquestion2.js
+++ testquestion2.js
@@ -1,1 +1,24 @@
+let square;
+
+function setup(){
+    new Canvas(400, 600);
+   
+    // code to set the gravity of the world
+    world.gravity.y = 10;
+
+    // code to create a new square sprite
+    square = new Sprite();
+    
+}
+
+function draw(){
+    background("grey");
+
+    // code to check if mouse is pressed i.e. mouse.presses()
+        // if mouse is pressed, the square sprite "flies" up
+    if (mouse.presses("left")) {
+        square.vel.y = -5;
+    }
+}
+
 
