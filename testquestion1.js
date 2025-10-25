35cc6c889f8908068663fe7cea0b55bd8b279998
MartinOng1 <90502210+MartinOng1@users.noreply.github.com>
2025-10-25 16:23:25 +0800

    Update testquestion1.js

--- testquestion1.js
+++ testquestion1.js
@@ -1,1 +1,21 @@
+let bird
+let background, birdImg;
+
+function preload(){
+    // write code to preload bird.png and background.png
+    // refer to assets folder
+    birdImg = loadImage("assets/bird.png");
+    background = loadImage("assets/background.png");
+}
+
+function setup(){
+    new Canvas(400, 600);
+    // write code to create sprite and load bird image on sprite
+    bird = new Sprite();
+    bird.img = birdImg;
+}
+
+function draw(){
+    image(background, 0, 0, width, height);
+}
 
